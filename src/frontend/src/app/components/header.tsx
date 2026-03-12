828aec9fded7f6aaeda37f40094632be0c27e587
arosario513 <a.rosario51304@gmail.com>
2026-03-12 15:09:56 -0400

    Init Commit

---
+++ src/frontend/src/app/components/header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <nav className="navbar navbar-expand bg-body-tertiary">
+            <div className="container-fluid">
+                <a className="navbar-brand">COMP-3600</a>
+            </div>
+        </nav>
+    );
+}
---
+++ src/frontend/src/app/layout.tsx
@@ -0,0 +1,26 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import type { Metadata } from "next";
+import "./globals.css";
+import Header from "./components/header";
+
+export const metadata: Metadata = {
+    title: "COMP-3600",
+    description:
+        "All my work from COMP-3600 (Computer Graphics) class is included here",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+            <body>
+                <Header />
+                <div className="container-fluid mt-3">{children}</div>
+            </body>
+        </html>
+    );
+}
